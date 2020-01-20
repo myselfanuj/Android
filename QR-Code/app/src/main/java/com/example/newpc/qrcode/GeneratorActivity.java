99eced51fb007fc4f475880eb4d668a261f711a3
aanuj67950 <aanuj67950@gmail.com>
2020-01-20 23:23:18 +0530

    QRCode Activity

---
+++ QR-Code/app/src/main/java/com/example/newpc/qrcode/GeneratorActivity.java
@@ -0,0 +1,47 @@
+package com.example.newpc.qrcode;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class GeneratorActivity extends AppCompatActivity {
+    EditText text;
+    Button gen_btn;
+    ImageView image;
+    String text2Qr;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generator);
+        text = (EditText) findViewById(R.id.text);
+        gen_btn = (Button) findViewById(R.id.gen_btn);
+        image = (ImageView) findViewById(R.id.image);
+        gen_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text2Qr = text.getText().toString().trim();
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try{
+                    BitMatrix bitMatrix = multiFormatWriter.encode(text2Qr, BarcodeFormat.QR_CODE,200,200);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    image.setImageBitmap(bitmap);
+                }
+                catch (WriterException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
---
+++ QR-Code/app/src/main/java/com/example/newpc/qrcode/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.newpc.qrcode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button gen, scan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gen = (Button) findViewById(R.id.gen);
+        scan = (Button) findViewById(R.id.scan);
+        gen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gIntent = new Intent(MainActivity.this, GeneratorActivity.class);
+                startActivity(gIntent);
+            }
+        });
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent rIntent = new Intent(MainActivity.this, ReaderActivity.class);
+                startActivity(rIntent);
+            }
+        });
+    }
+}
