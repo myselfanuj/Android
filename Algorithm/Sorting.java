f0e0de92de5f018994f3b9590bbdd0ed7378f379
Anuj Singh <43649349+myselfanuj@users.noreply.github.com>
2020-09-10 13:36:30 +0530

    Update Sorting.java

--- Algorithm/Sorting.java
+++ Algorithm/Sorting.java
@@ -1,1 +1,21 @@
+public class sorting{
+ public static void Main(String[] args){
+	Scanner scan = new Scanner(System.in);
+	int n = scan.nextInt();
+	int[] a = new int[n];
+	for(int i =0;i<n;i++){
+	a[i] = scan.nextInt();}
+	for(int i =0;i<n;i++){
+	  for(int j=0;j<n-1;j++){
+		if(a[j]>a[j+1]){
+int temp = a[j+1];
+	a[j+1]=a[j];
+	a[j]=temp;
+}
+else{
+break;}}}
 
+	for(int k=0;k<n;k++){
+	System.out.println(a[k] +"\n");}}}
+	
+	
